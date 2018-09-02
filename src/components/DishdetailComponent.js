abeca6a57dcc88a357d997ebb7100e742e1849be
light0617 <light11470617@gmail.com>
2018-09-02 14:19:06 -0700

    Presentational and Container Components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -7,15 +7,14 @@ class DishDetail extends Component {
     }
 
     renderComments(comments){
-        const monthName = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
-        "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-
         const CommentsSummary = comments.map((comment) => {
-            let comment_date = new Date(comment.date);
+            let comment_date = new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'})
+            .format(new Date(Date.parse(comment.date)));
+
             return(
                <ul className="list-unstyled"> 
                     <li>{comment.comment}</li>
-                    <li> - - {comment.author}, {monthName[comment_date.getMonth()]} {comment_date.getDate() + 1}, {comment_date.getFullYear()}</li>
+                    <li> - - {comment.author}, {comment_date}</li>
                </ul>
             );
         });
@@ -45,12 +44,14 @@ class DishDetail extends Component {
         }
 
         return (
-            <div className="row">
-                <div className="col-12 col-md-5 m-1">
-                    {dishDetail(dish)}
-                </div>
-                <div className="col-12 col-md-5 m-1">
-                    {comments(dish.comments)}
+            <div className="container">
+                <div className="row">
+                    <div className="col-12 col-md-5 m-1">
+                        {dishDetail(dish)}
+                    </div>
+                    <div className="col-12 col-md-5 m-1">
+                        {comments(dish.comments)}
+                    </div>
                 </div>
             </div>
         );
